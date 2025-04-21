cde5ec612d21b755227bb02024f5260b2e9b6d2f
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-04-21 19:12:00 +0530

    images add (pexels.com)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 // body of the function executes when the document is ready
 $(document).ready(function() {
-    $('#slides').superslides()
+    $('#slides').superslides({
+        Animation: 'fade',
+        play: 5000,
+        pagination: true,
+    })
 });
