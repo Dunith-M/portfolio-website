b52c278d2a900775364af70e70a19e80c160b4a8
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-04-23 12:45:55 +0530

    add skills

--- js/script.js
+++ js/script.js
@@ -4,22 +4,58 @@ $(document).ready(function() {
         Animation: 'fade',
         play: 5000,
         pagination: false,
-    })
+    });
 
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        items: 4,
         responsive:{
             0:{
                 items:1
             },
-            600:{
+            480:{
+                items:2
+            },
+            768:{
                 items:3
             },
-            1000:{
-                items:5
+            938:{
+                items:4
             }
         }
-    })
+    });
+
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 150,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+
+
+    var skillsTopOffset = $('.skillsSection').offset().top;
+    console.log(skillsTopOffset)
+
+    $(window).scroll(function() {
+       if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+        $('.chart').easyPieChart({
+            easing: 'easeInOut',
+            barColor: '#fff',
+            trackColor: false,
+            scaleColor: false,
+            lineWidth: 4,
+            size: 150,
+            onStep: function(from, to, percent) {
+                $(this.el).find('.percent').text(Math.round(percent));
+            }
+        })
+
+       }
+    });
+        
 });
