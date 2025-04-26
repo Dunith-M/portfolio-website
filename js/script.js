ec231b0aa21fcc12162482573c36f8d2b5a6c7e5
Dunith Munasinghe <113714394+Dunith-M@users.noreply.github.com>
2025-04-27 00:29:53 +0530

    portfolio section

--- js/script.js
+++ js/script.js
@@ -62,5 +62,15 @@ $(document).ready(function() {
     //$(".counter").countup(50);
 
     $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    
         
 });
